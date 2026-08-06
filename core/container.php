5e88e607b8ab922939223e750e3efb6fe93efb41
aliengll7520-maker <aliengll7520@gmail.com>
2026-08-06 10:32:51 +0700

    Update container.php

--- core/container.php
+++ core/container.php
@@ -1,1 +1,60 @@
+<?php
+/**
+ * K86SHOP 4.0
+ * Core Container
+ */
 
+defined('ABSPATH') || exit;
+
+if (!class_exists('K86SHOP_Container')) {
+
+    final class K86SHOP_Container
+    {
+        /**
+         * Shared services.
+         *
+         * @var array
+         */
+        private array $services = [];
+
+        /**
+         * Bind a service.
+         */
+        public function bind(string $name, object $service): void
+        {
+            $this->services[$name] = $service;
+        }
+
+        /**
+         * Get a service.
+         */
+        public function get(string $name): ?object
+        {
+            return $this->services[$name] ?? null;
+        }
+
+        /**
+         * Check service exists.
+         */
+        public function has(string $name): bool
+        {
+            return isset($this->services[$name]);
+        }
+
+        /**
+         * Return all services.
+         */
+        public function all(): array
+        {
+            return $this->services;
+        }
+    }
+}
+
+/*
+|--------------------------------------------------------------------------
+| Global Container
+|--------------------------------------------------------------------------
+*/
+
+$GLOBALS['k86shop_container'] = new K86SHOP_Container();

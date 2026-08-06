6bf11440f0aad9ed6affeefa2a2c1094f3b2bd15
aliengll7520-maker <aliengll7520@gmail.com>
2026-08-06 11:16:33 +0700

    Update container.php

--- core/container.php
+++ core/container.php
@@ -6,55 +6,22 @@
 
 defined('ABSPATH') || exit;
 
-if (!class_exists('K86SHOP_Container')) {
+/**
+ * Container.
+ */
+class K86SHOP_Container
+{
+    private $services = array();
 
-    final class K86SHOP_Container
+    public function set($key, $service)
     {
-        /**
-         * Shared services.
-         *
-         * @var array
-         */
-        private array $services = [];
+        $this->services[$key] = $service;
+    }
 
-        /**
-         * Bind a service.
-         */
-        public function bind(string $name, object $service): void
-        {
-            $this->services[$name] = $service;
-        }
-
-        /**
-         * Get a service.
-         */
-        public function get(string $name): ?object
-        {
-            return $this->services[$name] ?? null;
-        }
-
-        /**
-         * Check service exists.
-         */
-        public function has(string $name): bool
-        {
-            return isset($this->services[$name]);
-        }
-
-        /**
-         * Return all services.
-         */
-        public function all(): array
-        {
-            return $this->services;
-        }
+    public function get($key)
+    {
+        return isset($this->services[$key]) ? $this->services[$key] : null;
     }
 }
-
-/*
-|--------------------------------------------------------------------------
-| Global Container
-|--------------------------------------------------------------------------
-*/
 
 $GLOBALS['k86shop_container'] = new K86SHOP_Container();

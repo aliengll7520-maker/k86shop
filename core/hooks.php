d274395c3249162967cc0d606a33bb0e2ed11837
aliengll7520-maker <aliengll7520@gmail.com>
2026-08-06 10:30:42 +0700

    Update hooks.php

--- core/hooks.php
+++ core/hooks.php
@@ -1,1 +1,30 @@
+<?php
+/**
+ * K86SHOP 4.0
+ * Core Hooks
+ */
 
+defined('ABSPATH') || exit;
+
+if (!function_exists('k86shop_register_hooks')) {
+
+    function k86shop_register_hooks(): void
+    {
+        // Admin
+        add_action('admin_menu', 'k86shop_admin_menu');
+
+        // Init
+        add_action('init', 'k86shop_init');
+
+        // Activation / Deactivation hooks
+    }
+
+}
+
+/*
+|--------------------------------------------------------------------------
+| Register Hooks
+|--------------------------------------------------------------------------
+*/
+
+k86shop_register_hooks();
